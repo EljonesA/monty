5ea175b0bde5a542e0941c1c00319159ab2864d1
Elly <122857130+EljonesA@users.noreply.github.com>
2023-08-23 09:27:03 +0000

    Monty program main.c

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+
+/**
+ * main - runs the monty program
+ * @argc: argument count
+ * @argv: argument strings
+ *
+ * Return: 0 (Always success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	FILE *file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	char *line = NULL;
+	size_t len = 0;
+	int line_number = 0;
+
+	while (getline(&line, &len, file) != -1)
+	{
+		line_number++; /* make line_number 1 to indicate first line */
+		char *opcode = strtok(line, "\t\n");
+
+		if (opcode == NULL || opcode[0] == '\n' || opcode[0] == '#')
+		{
+			continue; /* ignore empty\blank lines, comments */
+		}
+
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
+
+	fclose(file);
+	free(line);
+
+	return (0);
+}
